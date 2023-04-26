04f51be2cdd8ec4b6fd1178cba1b026c6ee61537
Sayuri <sayurikamble123@gmail.com>
2023-04-26 12:43:12 +0530

    Switched to MantineUi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,33 @@ import {AudiogramProvider} from "@/contexts/audiogramContext";
 import "@/styles/globals.css";
 import "../remotion/style.css";
 import type {AppProps} from "next/app";
+import {MantineProvider} from "@mantine/core";
+import Head from "next/head";
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App(props: AppProps) {
+  const {Component, pageProps} = props;
   return (
-    <AudiogramProvider>
-      <Component {...pageProps} />
-    </AudiogramProvider>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <AudiogramProvider>
+          <Component {...pageProps} />
+        </AudiogramProvider>
+      </MantineProvider>
+    </>
   );
 }
