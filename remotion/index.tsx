f7a3be1aa8cfbb0939beb60c41e4d30871e80183
Sayuri <sayurikamble123@gmail.com>
2023-04-27 20:29:00 +0530

    Remotion Root for rendering

---
+++ remotion/index.tsx
@@ -0,0 +1,39 @@
+import {AudiogramProvider, useAudiogram} from "../contexts/audiogramContext";
+import {Composition, registerRoot} from "remotion";
+import {AudiogramComposition} from "./Composition";
+import "./style.css";
+
+export const RenderComposition = () => {
+  const {audiogramDetails} = useAudiogram();
+  return (
+    <>
+      <Composition
+        id="Audiogram"
+        component={AudiogramComposition}
+        durationInFrames={600}
+        fps={30}
+        width={audiogramDetails.orientation.compositionWidth}
+        height={audiogramDetails.orientation.compositionHeight}
+        defaultProps={{
+          audioOffsetInFrames: 0,
+          source: audiogramDetails.srtFile,
+          backgroundColor: audiogramDetails.designProps.backgroundColor,
+          textColor: audiogramDetails.designProps.textColor,
+          titleColor: audiogramDetails.designProps.titleColor,
+        }}
+      />
+    </>
+  );
+};
+
+export const RemotionVideo = () => {
+  return (
+    <>
+      <AudiogramProvider>
+        <RenderComposition />
+      </AudiogramProvider>
+    </>
+  );
+};
+
+registerRoot(RemotionVideo);
